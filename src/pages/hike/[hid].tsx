1b1f81b7818c452837d7f84c41fcb3a83d5a441f
Rohan Fernandes <59456400+proballstar@users.noreply.github.com>
2023-10-08 18:51:57 -0700

    Update [hid].tsx

--- src/pages/hike/[hid].tsx
+++ src/pages/hike/[hid].tsx
@@ -7,6 +7,7 @@ import { LoadingPage } from "~/components/loading";
 import Modal from "~/components/modal";
 import { api } from "~/utils/api";
 import Input from "~/components/input";
+import Head from 'next/head'
 
 export default function SpecificHikePage() {
     const router = useRouter()
@@ -38,36 +39,43 @@ export default function SpecificHikePage() {
     }
 
     return (
-        <div className="flex flex-col px-10 py-4">
-            <h1 className="text-4xl font-semibold capitalize">{data.post.name}</h1>
-            <div className="flex flex-col py-3">
-                <p>Created by {`${data.author.firstName} ${data.author.lastName}`}</p>
-                <p>{data.post.desc}</p>
-                <p>{data.post.date?.toDateString()}</p>
-                <p>Number of attendees: {data.post.Attendees.length}</p>
-                <div>
-                    <h2 className="font-semibold">Attendees</h2>
-                    <div className="flex flex-col">
-                        {data.post.Attendees.map((attendee) => {
-                            const fullName = `${attendee.Person!.fname!} ${attendee.Person!.lname!}`
-                            return (
-                                <div className="flex flex-row" key={`Attendee-${attendee.Person?.fname}-${attendee.Person?.lname}`}>
-                                    {fullName}: {attendee.comment}
-                                </div>
-                            )
-                        })}
+        <>
+            <Head>
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5539865102402934"
+     crossorigin="anonymous"></script>
+            </Head>
+            <div className="flex flex-col px-10 py-4">
+                <h1 className="text-4xl font-semibold capitalize">{data.post.name}</h1>
+                <div className="flex flex-col py-3">
+                    <p>Created by {`${data.author.firstName} ${data.author.lastName}`}</p>
+                    <p>{data.post.desc}</p>
+                    <p>{data.post.date?.toDateString()}</p>
+                    <p>Number of attendees: {data.post.Attendees.length}</p>
+                    <div>
+                        <h2 className="font-semibold">Attendees</h2>
+                        <div className="flex flex-col">
+                            {data.post.Attendees.map((attendee) => {
+                                const fullName = `${attendee.Person!.fname!} ${attendee.Person!.lname!}`
+                                return (
+                                    <div className="flex flex-row" key={`Attendee-${attendee.Person?.fname}-${attendee.Person?.lname}`}>
+                                        {fullName}: {attendee.comment}
+                                    </div>
+                                )
+                            })}
+                        </div>
                     </div>
+                    <a href={data.post.route!} className="flex justify-center px-6 py-2 border-2 border-slate-600 rounded-md">
+                        Route
+                    </a>
+                    <button onClick={() => setModalOpen(true)} data-modal-target="defaultModal" data-modal-toggle="defaultModal" className="px-6 py-2 bg-green-500 my-2 rounded-lg text-white">
+                        Join Event
+                    </button>
+                    <Modal header="Join Event" sub_message={joinLoading ? "Loading..." : "Submit"} close={() => modalClose()} open={() => setModalOpen(true)} isOpen={modalOpen}>
+                        <Input name="Comments" placeholder="Enter any comments here" value={comments} onChange={(e) => setComments(e.target.value)}  />
+                    </Modal>
                 </div>
-                <a href={data.post.route!} className="flex justify-center px-6 py-2 border-2 border-slate-600 rounded-md">
-                    Route
-                </a>
-                <button onClick={() => setModalOpen(true)} data-modal-target="defaultModal" data-modal-toggle="defaultModal" className="px-6 py-2 bg-green-500 my-2 rounded-lg text-white">
-                    Join Event
-                </button>
-                <Modal header="Join Event" sub_message={joinLoading ? "Loading..." : "Submit"} close={() => modalClose()} open={() => setModalOpen(true)} isOpen={modalOpen}>
-                    <Input name="Comments" placeholder="Enter any comments here" value={comments} onChange={(e) => setComments(e.target.value)}  />
-                </Modal>
+                
             </div>
-        </div>
+        </>
     )
 }
