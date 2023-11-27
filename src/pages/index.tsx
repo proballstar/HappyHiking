ca9d66822c037bee763a26b03c1733aac506e8d2
Rohan Fernandes <59456400+proballstar@users.noreply.github.com>
2023-11-26 23:10:56 -0800

    bug fixes 1

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import { api } from "~/utils/api";
-import RedesignedHeader from "./components/redesigned/header";
-import PostFeeder from "./components/redesigned/post.feeder";
+import RedesignedHeader from "../components/redesigned/header";
+import PostFeeder from "../components/redesigned/post.feeder";
 
 export default function Redesign() {
 
